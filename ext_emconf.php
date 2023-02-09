ca31a48db60a503a5b9f69bc30169c4a55a49ebd
Christian <werbegrafik@rc-design.at>
2023-02-09 14:02:35 +0100

    Update change depends and require for v12 make a t3 update v11.5.23

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-12.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
